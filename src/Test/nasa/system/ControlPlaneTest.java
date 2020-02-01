fbeaeeb106351627b5aad36cd113a5b2e6aa5f1b
jeff wu <j98728@hotmail.com>
2020-02-01 17:26:23 +0000

    Added new features

---
+++ src/Test/nasa/system/ControlPlaneTest.java
@@ -0,0 +1,18 @@
+package nasa.system;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControlPlaneTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void test(){
+
+    }
+}
---
+++ src/Test/nasa/system/robot/RoverTest.java
@@ -0,0 +1,63 @@
+package nasa.system.robot;
+
+import nasa.system.airspace.Explorable;
+import nasa.system.command.Command;
+import nasa.system.compass.Heading;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoverTest {
+    Rover rover;
+
+    @BeforeEach
+    void setUp() {
+        int initPosX = 0;
+        int initPosY = 0;
+        Heading initHeading = Heading.N;
+        Explorable explorable = null;
+        List<String> actions = new ArrayList<>();
+        rover = new Rover(initPosX, initPosY, initHeading, explorable, actions);
+    }
+
+    @Test
+    void moveForward() {
+    }
+
+    @Test
+    void turnLeft() {
+    }
+
+    @Test
+    void turnRight() {
+    }
+
+    @Test
+    void incrX() {
+        rover.incrX();
+        Assertions.assertEquals(1, rover.getPosX());
+    }
+
+    @Test
+    void decrX_out_of_bound() {
+        rover.decrX();
+        Assertions.assertTrue(rover.getPosX() < 0);
+    }
+
+    @Test
+    void incrY() {
+        rover.incrY();
+        Assertions.assertEquals(1, rover.getPosY());
+    }
+
+    @Test
+    void decrY_out_of_bound() {
+        rover.decrY();
+        Assertions.assertTrue(rover.getPosY() < 0);
+    }
+}
